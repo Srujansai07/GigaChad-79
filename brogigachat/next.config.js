63507f0cf2a9a6fd08ed949154560fbd169766b3
Srujansai07 <160607915+Srujansai07@users.noreply.github.com>
2025-12-11 20:55:09 +0530

    Add brogigachat/next.config.js

---
+++ brogigachat/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    // Enable if using images from external sources
+    // images: {
+    //   domains: ['example.com'],
+    // },
+};
+
+module.exports = nextConfig;
